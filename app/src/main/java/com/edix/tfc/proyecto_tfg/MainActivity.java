9c9520020327e693196688d2b1d3f334bd765555
sergiopalmero <sergiopalmero87@gmail.com>
2024-03-22 10:34:24 +0100

    Creado proyecto

---
+++ app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.edix.tfc.proyecto_tfg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
