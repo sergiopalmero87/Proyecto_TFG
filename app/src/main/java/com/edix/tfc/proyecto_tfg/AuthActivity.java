a23b08412e23409d146a9877fc983d2e92b82916
sergiopalmero <sergiopalmero87@gmail.com>
2024-03-22 11:28:56 +0100

    implementacion firebase auth y creado activity_auth.xml

---
+++ app/src/main/java/com/edix/tfc/proyecto_tfg/AuthActivity.java
@@ -0,0 +1,15 @@
+package com.edix.tfc.proyecto_tfg;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
+
