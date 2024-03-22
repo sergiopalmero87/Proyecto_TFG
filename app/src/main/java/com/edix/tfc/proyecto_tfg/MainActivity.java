d50a3564a086aeb65c7231ff94b371eb200b8c2a
sergiopalmero <sergiopalmero87@gmail.com>
2024-03-22 13:19:18 +0100

    Lottie Files. Anim cerrar sesion. Funcion cerrar sesion.

--- app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
+++ app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
@@ -2,13 +2,45 @@ package com.edix.tfc.proyecto_tfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LottieAnimationView logOutButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        logOutButton = findViewById(R.id.logOut);
+
+        // Establecer el OnClickListener para el botón de salida
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Reproducir la animación
+                logOutButton.playAnimation();
+
+                // Cerrar sesión después de un breve retraso
+                new android.os.Handler().postDelayed(
+                        new Runnable() {
+                            public void run() {
+                                mAuth.signOut();
+                                startActivity(new Intent(MainActivity.this, AuthActivity.class));
+                                finish();
+                            }
+                        },
+                        1000 // Retraso de 1 segundo para dar tiempo a la animación
+                );
+            }
+        });
     }
 }
