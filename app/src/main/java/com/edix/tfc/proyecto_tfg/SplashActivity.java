3ec5133264ddc2a2a196608fc23d5c2ce24b9439
sergiopalmero <sergiopalmero87@gmail.com>
2024-04-01 11:34:08 +0200

    2 json para splash y SplashActivity

---
+++ app/src/main/java/com/edix/tfc/proyecto_tfg/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.edix.tfc.proyecto_tfg;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+
+}
