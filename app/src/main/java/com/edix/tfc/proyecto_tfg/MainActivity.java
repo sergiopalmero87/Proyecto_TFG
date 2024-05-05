07db9979a62916b4fece5d03dcfa6665f673f663
sergiopalmero <sergiopalmero87@gmail.com>
2024-05-05 12:04:40 +0200

    Prueba retrofit y cards funcionan

--- app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
+++ app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
@@ -3,17 +3,28 @@ package com.edix.tfc.proyecto_tfg;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.airbnb.lottie.LottieAnimationView;
+import com.edix.tfc.proyecto_tfg.retrofit.interfac.RetrofitApi;
 import com.edix.tfc.proyecto_tfg.retrofit.modelo.ListAdapter;
 import com.edix.tfc.proyecto_tfg.retrofit.modelo.ListElement;
+import com.edix.tfc.proyecto_tfg.retrofit.modelo.Noticias;
 import com.google.firebase.auth.FirebaseAuth;
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,10 +47,11 @@ public class MainActivity extends AppCompatActivity {
 
         iniciarVariables();
         recyclerViewLayoutManager();
-        mostrarTarjetas();
+
         logOut();
         config();
         verNoticiasGuardadas();
+        respuestaRetrofit();
 
     }
 
@@ -62,24 +74,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void mostrarTarjetas(){
-        // lista de elementos de ejemplo
-        itemList = new ArrayList<>();
-        itemList.add(new ListElement("Este texto es de prueba para las noticias para ver como se adapta a la card"));
-        itemList.add(new ListElement("okalsnjdkaksdmñjalkdklañndjmkalsndklasdnm´kañsjdjañklksjdmañjs-kn.dmajslmdbnakjslbdakjslbdajklbsdjaksbdjaksndkjasndkalñnsdalkñsndl"));
-        itemList.add(new ListElement("Noticia 3"));
-        itemList.add(new ListElement("Noticia 4"));
-        itemList.add(new ListElement("Este texto es de prueba para las noticias para ver como se adapta a la card"));
-        itemList.add(new ListElement("okalsnjdkaksdmñjalkdklañndjmkalsndklasdnm´kañsjdjañklksjdmañjs-kn.dmajslmdbnakjslbdakjslbdajklbsdjaksbdjaksndkjasndkalñnsdalkñsndl"));
-        itemList.add(new ListElement("Noticia 7"));
-        itemList.add(new ListElement("Noticia 8"));
-
-        //Metemos en el adapter la lista de elementos a mostrar
-        listAdapter = new ListAdapter(this, itemList);
-
-        //Seteamos el adapter dentro del recyclerView(contenedor)
-        recyclerView.setAdapter(listAdapter);
-    }
 
     private void logOut () {
         logOutButton.setOnClickListener(new View.OnClickListener() {
@@ -132,5 +126,52 @@ public class MainActivity extends AppCompatActivity {
             );
         });
     }
+
+    private void respuestaRetrofit() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitApi retrofitApi = retrofit.create(RetrofitApi.class);
+
+        Call<List<Noticias>> respuesta = retrofitApi.getPosts();
+
+        respuesta.enqueue(new Callback<List<Noticias>>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(Call<List<Noticias>> call, Response<List<Noticias>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Error al obtener noticias", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                itemList = new ArrayList<>();
+
+                List<Noticias> noticias = response.body();
+                for (Noticias noticia : noticias) {
+                    String content = "";
+                    content += "userId" + noticia.getUserId() + "\n";
+                    content += "id" + noticia.getId() + "\n";
+                    content += "title" + noticia.getTitle() + "\n";
+                    content += "body" + noticia.getBody() + "\n";
+
+                    // Agregar el contenido de la noticia a itemList
+                    itemList.add(new ListElement(content));
+                    listAdapter = new ListAdapter(MainActivity.this, itemList);
+                    recyclerView.setAdapter(listAdapter);
+                }
+
+                // Notificar al adaptador que los datos han cambiado
+                listAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Noticias>> call, Throwable throwable) {
+                Toast.makeText(MainActivity.this, "Error en la solicitud de Retrofit", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 
 }
