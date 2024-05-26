07e42e3e2e0d8dbbb76391bf85dc2865753ceffb
sergiopalmero <sergiopalmero87@gmail.com>
2024-05-26 15:49:19 +0200

    Probado en movil. Ajustes

--- app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
+++ app/src/main/java/com/edix/tfc/proyecto_tfg/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,6 +27,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -55,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getActionBar()).hide();
         setContentView(R.layout.activity_main);
 
         iniciarVariables();
